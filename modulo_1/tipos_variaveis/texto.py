cc1edcfe9338a9351a2c57c88ad74fec15ff1d51
Anderson Fernandes <anderson.fernandes96@outlook.com>
2025-02-06 21:32:39 -0300

    Booleanos

--- modulo_1/tipos_variaveis/texto.py
+++ modulo_1/tipos_variaveis/texto.py
@@ -1,12 +1,24 @@
 # Declaração
 nome_completo = "Gabriel Casemiro"
 
-nome_completo = """Gabriel
+nome_completo_aspas = """Gabriel
 Casemiro
 """
 
-nome_completo = "Gabriel \
+nome_completo_quebra = "Gabriel \
 Casemiro"
 
 nome = "Gabriel"
 sobrenome = "Casemiro"
+
+# Formatação
+print("Nome completo (1ª forma):", nome_completo)
+print("Nome completo (2ª forma):" + nome_completo)
+print("Nome completo (3ª forma):" + "Gabriel" + "Casemiro")
+print("Nome completo (4ª forma):" + "Gabriel", "Casemiro")
+print("Nome completo (5ª forma):", nome_completo_aspas)
+print("Nome completo (6ª forma):", nome_completo_quebra)
+print("Nome completo (7ª forma): %s" % nome_completo)
+print("Nome completo (8ª forma): %s %s" % (nome, sobrenome))
+print(f"Nome completo (9ª forma): {nome} {sobrenome}")
+print("Nome completo (10ª forma): {} {}".format(sobrenome, nome))
