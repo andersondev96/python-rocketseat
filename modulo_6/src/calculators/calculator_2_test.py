53aba78549881db91e70e7006266c960ed13e712
Anderson Fernandes <anderson.fernandes96@outlook.com>
2025-10-21 22:41:42 -0300

    Project interfaces apply

--- modulo_6/src/calculators/calculator_2_test.py
+++ modulo_6/src/calculators/calculator_2_test.py
@@ -1,5 +1,7 @@
 from src.calculators.calculator_2 import Calculator2
 from typing import Dict
+from src.drivers.numpy_handler import NumpyHandler
+
 
 
 class MockRequest:
@@ -12,7 +14,8 @@ def test_calculate():
       "numbers": [2.12, 4.62, 1.32] 
     })
 
-  calculator_2 = Calculator2()
+  driver = NumpyHandler()
+  calculator_2 = Calculator2(driver)
   formated_response = calculator_2.calculate(mock_request)
 
   assert isinstance(formated_response, dict)  
